098271c295ce5b4962eed136aac0cd2daa6688df
zeromagic46 <122884018+zeromagic46@users.noreply.github.com>
2026-08-14 21:49:45 +0800

    Delete javdb_startup.js

--- javdb_startup.js
+++
@@ -1,6 +0,0 @@
-var obj = JSON.parse($response.body);
-if (obj.data && obj.data.splash_ad) {
-  obj.data.splash_ad.enabled = false;
-  obj.data.splash_ad.ad = null;
-}
-$done({ body: JSON.stringify(obj) });
